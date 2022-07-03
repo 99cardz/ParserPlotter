5db4ffa05392d3afdc59f0856b2e1dfd631371d1
99cardz <jonathan.schulze.de@gmail.com>
2022-07-03 19:28:08 +0200

    calculating y values for all x values  calculated from each horizontal pixel seems to get thighter graphs

--- src/CanvasPlot.java
+++ src/CanvasPlot.java
@@ -18,24 +18,32 @@ public class CanvasPlot extends Canvas {
 		centerX = width / 2;
 		centerY = height / 2;
 	}
+	public void setScale(int factorX, int factorY) {
+		scaleX = factorX != 0 ? scaleX * factorY : scaleX;
+		scaleY = factorY != 0 ? scaleY * factorY : scaleY;
+		
+		invalidate();
+	}
 	
 	public void paint(Graphics g) {
 		Dimension dims = getSize();
 		
 		// smallest and largest visible x and y value of coordinate system
-		int minX = -(centerX / scaleX); 
-		int maxX = (dims.width - centerX) / scaleX;
-		int minY = -(dims.height - centerY) / scaleY; 
-		int maxY = centerY / scaleY;
+		double minX = -((double) centerX / scaleX); 
+		double maxX = ((double) (dims.width - centerX)) / scaleX;
+		double minY = (-(double) (dims.height - centerY)) / scaleY; 
+		double maxY = ((double) centerY) / scaleY;
+		
+//		System.out.println("minX: " + minX + " maxX: " + maxX + " minY: " + minY + " maxY: " + maxY);
 		
 		// paint value indicator lines
 		g.setColor(Color.lightGray);
-		for (int x = minX; x <= maxX; x++) {
+		for (double x = Math.ceil(minX); x < maxX; x++) {
 			g.drawLine(toXCoord(x), 0, toXCoord(x), dims.height);
 //			String str = toString(x);
 //			g.drawString(str, toXCoord(x) - str.length() * 4, centerY - 10);
 		}
-		for (int y = minY; y <= maxY; y++)
+		for (double y = Math.ceil(minY); y < maxY; y++)
 			g.drawLine(0, toYCoord(y), dims.width, toYCoord(y));
 		
 		// paint axies
@@ -43,54 +51,26 @@ public class CanvasPlot extends Canvas {
 		thickLine(g, 0, centerY, dims.width, centerY); // x
 		thickLine(g, centerX, 0, centerX, dims.height); // y
 		
-		// paint value indicator numbers
-		
-		
-		// get lowest and highest visible x value of Graph
-		double minXVisible = minX;
-		while(minXVisible < maxX && (f(minXVisible) < minY || f(minXVisible) > maxY))
-			minXVisible += 0.2;
-		double maxXVisible = maxX;
-		while(maxXVisible > minX && (f(maxXVisible) < minY || f(maxXVisible) > maxY))
-			maxXVisible -= 0.2;
-		
-		// give some space
-		minXVisible = Math.floor(minXVisible) - 1; 
-		maxXVisible = Math.ceil(maxXVisible) + 1; 
-		double visibleXRange = maxXVisible - minXVisible;
-		if (visibleXRange <= 0) 
-			return;
-		
-		// determine accuracy: every fourth px gets a point, but not above 1000
-		int pointCount = (int) (visibleXRange * scaleX/4);
-		int accuracy = pointCount > 1000 ? 1000 : pointCount;
-		
-		
 		// calculate coords for graph
-		int[] xCoords = new int[accuracy];
-		int[] yCoords = new int[accuracy];
-		double stepX = visibleXRange / accuracy;
-		System.out.println(stepX);
-		for (int i = 0; i < accuracy; i++) {
-			double xValue = minXVisible + stepX * i;
-			xCoords[i] = toXCoord(xValue);
-			yCoords[i] = toYCoord(f(xValue));
-			System.out.println(xValue);
-		}
+		int[] yCoords = new int[dims.width];
+		for (int xCoord = 0; xCoord < dims.width; xCoord++)
+			yCoords[xCoord] = toYCoord(f(toXValue(xCoord)));
 		
 		// paint graph
 		g.setColor(Color.blue);
-		for (int i = 1; i < accuracy; i++)
-			thickLine(g, xCoords[i-1], yCoords[i-1], xCoords[i], yCoords[i]);
+		for (int xCoord = 1; xCoord < dims.width; xCoord++) 
+			if (Math.abs(yCoords[xCoord-1] - yCoords[xCoord]) < dims.height)
+				thickLine(g, xCoord-1, yCoords[xCoord-1], xCoord, yCoords[xCoord]);
 	}
 	private int toXCoord(double x) { return (int) (centerX + x * scaleX); }
 	private int toYCoord(double y) { return (int) (centerY - y * scaleY); }
-	private String toString(int a) { return "" + a; }
+	private double toXValue(int x) { return ((double) (x - centerX)) / scaleX; }
+	private String toString(int a) { return String.valueOf(a); }
 	
 	// to be replaced by Parser.eval()
 	private double f(double x) {
 //		return Math.cos(x);
-		return 0.1*x*x-4;
+		return x*x*x - 3*x;
 	}
 	private void thickLine(Graphics g, int x1, int y1, int x2, int y2) {
 		g.drawLine(x1, y1-1, x2, y2-1);
