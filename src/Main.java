e7b1f05fcdec25e070009556a013661ff926d6ae
nessel <w.2nert@gmail.com>
2022-07-03 16:26:21 +0200

    frame formatiert, allerdings will alles noch nicht aktuell noch nicht

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,11 @@ import java.awt.Frame;
 
 public class Main {
 	
-	final Frame window = new Frame("Funktionsplotter");
-	
-	Main() {
-		window.add(new CanvasPlot());
-		window.setLayout(null);    
-		window.setSize(400, 400);    
-		window.setVisible(true); 
-	}
-
 	public static void main(String[] args) {
 		
-		Main programm = new Main();
+		CanvasPlot plot = new CanvasPlot();
+		Window window = new Window(plot);
+		window.setVisible(true);
 	}
 
 }
--- src/Window.java
+++ src/Window.java
@@ -1,65 +1,143 @@
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
 import javax.swing.*;
+import javax.swing.event.*;
 
 public class Window extends JFrame {
 	
-	final Color bgColor = Color.orange;
+	// beauty stuff
+	private final Color 	BG_COLOR = Color.orange;
+	private Font 			mathFont = new Font("Times New Roman", Font.BOLD, 25);
+	private Font 			genericFont = new Font("Arial", Font.PLAIN, 25);
 	
-	JPanel upperPanel = new JPanel();
-	JLabel funcLabel = new JLabel("f(x) =");
-	JTextField textInput = new JTextField("");
-	JButton quitButton = new JButton("Button");
-	JLabel errorLabel = new JLabel("Error.");
+	// layout
+	private final Dimension MIN_WINDOW_SIZE = new Dimension(600, 800);
+	private final Dimension BUTTON_SIZE = new Dimension(50, 50);
+	private final int 		TOP_HEIGHT = 60;
+	private final int		ERR_HEIGHT = 40;
+	private final int		BOT_HEIGHT = TOP_HEIGHT;
+ 	
+	// components
+	private JPanel 			topPanel = new JPanel();
+	private JLabel 			funcLabel = new JLabel("f(x) =");
+	private JTextField 		textInput = new JTextField("");
 	
-	JPanel canvasPanel = new JPanel();
-	Canvas canvas;
+	private JPanel			errorPanel = new JPanel();
+	private JLabel 			errorLabel = new JLabel("Error.");
 	
-	JPanel lowerPanel = new JPanel();
-	JSlider xSlider = new JSlider();
-	JSlider ySlider = new JSlider();
+	private JPanel 			canvasPanel = new JPanel();
+	private Canvas 			canvas;
 	
-	Font mathFont = new Font("Times New Roman", Font.BOLD, 25);
-	Font genericFont = new Font("Arial", Font.PLAIN, 25);
+	private JPanel 			bottomPanel = new JPanel();
+	private double[]		currScale = {1.0, 1.0};
+	private JLabel			scaleLabel = new JLabel("x: " + currScale[1] + " y: " + currScale[1]);
+	private JButton 		xZoomOutButton = new JButton("-");
+	private JButton 		xZoomInButton = new JButton("+");
+	private JButton 		yZoomOutButton = new JButton("-");
+	private JButton 		yZoomInButton = new JButton("+");
+	private JButton			resetButton = new JButton("R");
+	
 	
 	Window(Canvas c) {
 		
 		this.canvas = c;
 		
 		// window setup
-		this.setSize(600, 400);
-		this.setTitle("Funky Funky");
+		this.setSize(MIN_WINDOW_SIZE);
+		this.setTitle("Funky");
 		this.setLayout(new BorderLayout());
+		this.setResizable(true);
+		this.setMinimumSize(MIN_WINDOW_SIZE);
 		
 		// upper panel setup
-		upperPanel.setLayout(null);
-		upperPanel.setBounds(0, 0, this.getWidth(), 70);
-		upperPanel.setBackground(bgColor);
-		upperPanel.add(textInput);
-		upperPanel.add(errorLabel);
-		upperPanel.add(funcLabel);
+		topPanel.setLayout(null);
+		topPanel.setBounds(0, 0, this.getWidth(), TOP_HEIGHT);
+		topPanel.setBackground(BG_COLOR);
 		funcLabel.setBounds(20, 20, 70, 20);
 		textInput.setBounds(funcLabel.getX() + funcLabel.getWidth(), funcLabel.getY() - 15, 200, 50);
 		errorLabel.setBounds(400, 100, 20, 50);
 		funcLabel.setFont(mathFont);
 		textInput.setFont(genericFont);
+		textInput.getDocument().addDocumentListener(new DocumentListener() {
+			  public void changedUpdate(DocumentEvent e) {
+				  System.out.println("Veränderung");
+			  }
+			  public void removeUpdate(DocumentEvent e) {
+				  changedUpdate(e);
+			  }
+			  
+			  public void insertUpdate(DocumentEvent e) {
+				  changedUpdate(e);
+			  }
+		});
+		scaleLabel.setBounds(200, 20, 20, 60);
+		scaleLabel.setBackground(Color.black);
+		topPanel.add(textInput);
+		topPanel.add(funcLabel);
+		topPanel.add(scaleLabel);
 		
-		// canvas panel
+		// error panel setup
+		errorPanel.setLayout(null);
+		errorPanel.setBackground(Color.LIGHT_GRAY);
+		errorPanel.add(errorLabel);
+		
+		// canvas panel setup
 		canvasPanel.setLayout(null);
-		canvasPanel.setBounds(0, upperPanel.getX() + upperPanel.getHeight(), this.getWidth(), this.getHeight() - 150);
-		canvasPanel.setBackground(Color.white);
+//		canvasPanel.setBounds(0, TOP_HEIGHT + ERR_HEIGHT, this.getWidth(), this.getHeight() - TOP_HEIGHT - ERR_HEIGHT - BOT_HEIGHT);
+		canvasPanel.setBackground(Color.orange);
 		canvasPanel.add(canvas);
 		
-		// lower panel setup
-		lowerPanel.setLayout(null);
-		lowerPanel.setBounds(0, canvasPanel.getX() + canvasPanel.getHeight(), this.getWidth(), this.getHeight() - 200);
-		lowerPanel.setBackground(bgColor);
-		xSlider = new JSlider(JSlider.HORIZONTAL, 1, 10, 1);
-		xSlider.setBounds(50, lowerPanel.getY() + 50, 100, 20);
-		lowerPanel.add(xSlider);
+		// bottom panel setup
+		bottomPanel.setBackground(Color.blue);
+		JButton[] buttons = {xZoomOutButton, xZoomInButton, yZoomOutButton, yZoomInButton, resetButton};
+		double[][] scalars = {{10.0, 1.0}, {0.1, 1.0}, {1.0, 10.0}, {1.0, 0.1}, {0.0, 0.0}};
+		for(int i = 0; i < buttons.length; i++) {
+			buttons[i].setBounds(20 + (i*(BUTTON_SIZE.width + 20)), bottomPanel.getY() + 20, BUTTON_SIZE.width, BUTTON_SIZE.height);
+			final int j = i;
+			buttons[i].addActionListener(e -> updateScale(scalars[j][0], scalars[j][1]));
+			bottomPanel.add(buttons[i]);
+		}
 		
 		// final adds
-		this.add(upperPanel);
+		this.updateBounds();
+		this.add(topPanel);
 		this.add(canvasPanel);
-		this.add(lowerPanel);
+		this.add(bottomPanel);
+		this.addComponentListener(new ComponentAdapter() {
+		    public void componentResized(ComponentEvent componentEvent) {
+		        updateBounds();
+		    }
+		});
+	}
+	
+	private void updateScale(double x, double y) {
+		
+		if(x == 0 && y == 0) {
+			currScale[0] = 1.0;
+			currScale[1] = 1.0;
+		}
+		else {
+			currScale[0] *= x;
+			currScale[1] *= y;
+		}
+		// noch zu implementieren
+		// canvas.setScale(x, y);
+		scaleLabel.setText("x: " + currScale[1] + " y: " + currScale[1]);
+		System.out.println("x: " + currScale[1] + " y: " + currScale[1]);
+		this.repaint();
+	}
+	
+	private void updateBounds() {
+		topPanel.setBounds		(0, 0, 
+								this.getWidth(), TOP_HEIGHT);
+		errorPanel.setBounds	(0, TOP_HEIGHT,
+								this.getWidth(), ERR_HEIGHT);
+		canvasPanel.setBounds	(0, TOP_HEIGHT + ERR_HEIGHT,
+								this.getWidth(), this.getHeight() - TOP_HEIGHT - ERR_HEIGHT - BOT_HEIGHT);
+		bottomPanel.setBounds	(0, this.getHeight() - BOT_HEIGHT,
+								this.getWidth(), BOT_HEIGHT);
+		repaint();
 	}
 }
