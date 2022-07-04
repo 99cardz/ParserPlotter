0225cfb9db62048e256ed2e589cc8cb757872421
99cardz <jonathan.schulze.de@gmail.com>
2022-07-04 10:18:06 +0200

    yCoords get calculated as they are drawn, no allocation needed

--- src/CanvasPlot.java
+++ src/CanvasPlot.java
@@ -9,20 +9,22 @@ public class CanvasPlot extends Canvas {
 //	final Parser parser = new Parser();
 	
 	private int centerX, centerY;
-	private int scaleX = 30, scaleY = 30; // pixels per unit
+	private int scaleX, scaleY; // pixels per unit
 	
-	final int DEFAULT_SCALE = 30;
+	static final int DEFAULT_SCALE = 40;
 
 	CanvasPlot(int width, int height) {
 		setBackground(Color.white);
-		setSize(width,height);
+		setSize(width, height);
 		// center is in middle of canvas by default
 		centerX = width / 2;
 		centerY = height / 2;
+		scaleX = DEFAULT_SCALE;
+		scaleY = DEFAULT_SCALE;
 	}
-	public void scale(int factorX, int factorY) {
-		scaleX = factorX != 0 ? scaleX * factorX : scaleX;
-		scaleY = factorY != 0 ? scaleY * factorY : scaleY;
+	public void scale(double factorX, double factorY) {
+		scaleX = (int) (factorX != 0 ? scaleX * factorX : scaleX);
+		scaleY = (int) (factorY != 0 ? scaleY * factorY : scaleY);
 		invalidate();
 	}
 	public void resetScale() {
@@ -32,7 +34,7 @@ public class CanvasPlot extends Canvas {
 	}
 	public void offset(int offsetX, int offsetY) {
 		centerX += scaleX * offsetX;
-		centerY += scaleY * offsetY;
+		centerY += scaleY * -offsetY;
 		invalidate();
 	}
 	public void resetOffset() {
@@ -42,49 +44,47 @@ public class CanvasPlot extends Canvas {
 	}
 	
 	public void paint(Graphics g) {
-		Dimension dims = getSize();
-		
-		// smallest and largest visible x and y value of coordinate system
-		double minX = -((double) centerX / scaleX); 
-		double maxX = ((double) (dims.width - centerX)) / scaleX;
-		double minY = (-(double) (dims.height - centerY)) / scaleY; 
-		double maxY = ((double) centerY) / scaleY;
-		
-//		System.out.println("minX: " + minX + " maxX: " + maxX + " minY: " + minY + " maxY: " + maxY);
+		int w = getWidth();
+		int h = getHeight();
 		
 		// paint value indicator lines
 		g.setColor(Color.lightGray);
-		for (double x = Math.ceil(minX); x < maxX; x++)
-			g.drawLine(toXCoord(x), 0, toXCoord(x), dims.height);
-		for (double y = Math.ceil(minY); y < maxY; y++)
-			g.drawLine(0, toYCoord(y), dims.width, toYCoord(y));
+		int maxX = (int) toXValue(w);
+		for (int x = (int) toXValue(0); x <= maxX; x++)
+			g.drawLine(toXCoord(x), 0, toXCoord(x), h);
+		int maxY = (int) toYValue(0);
+		for (int y = (int) toYValue(h); y <= maxY; y++)
+			g.drawLine(0, toYCoord(y), w, toYCoord(y));
 		
 		// paint axies
 		g.setColor(Color.black);
-		thickLine(g, 0, centerY, dims.width, centerY); // x
-		thickLine(g, centerX, 0, centerX, dims.height); // y
-		
-		// calculate coords for graph
-		int[] yCoords = new int[dims.width];
-		for (int xCoord = 0; xCoord < dims.width; xCoord++)
-			yCoords[xCoord] = toYCoord(f(toXValue(xCoord)));
+		thickLine(g, 0, centerY, w, centerY); // x
+		thickLine(g, centerX, 0, centerX, h); // y
 		
 		// paint graph
 		g.setColor(Color.blue);
-		for (int xCoord = 1; xCoord < dims.width; xCoord++) 
-			if (Math.abs(yCoords[xCoord-1] - yCoords[xCoord]) < dims.height)
-				thickLine(g, xCoord-1, yCoords[xCoord-1], xCoord, yCoords[xCoord]);
+		int prevY = toYCoord(f(toXValue(0)));
+		for (int xCoord = 1; xCoord < w; xCoord++) {
+			int currY = toYCoord(f(toXValue(xCoord)));
+			thickLine(g, xCoord-1, prevY, xCoord, currY);
+			prevY = currY;
+		}
 	}
 	private int toXCoord(double x) { return (int) (centerX + x * scaleX); }
-	private int toYCoord(double y) { return (int) (centerY - y * scaleY); }
+	private int toYCoord(double y) { 
+		int coord = (int) (centerY - y * scaleY);
+		if (coord < 0) return -3;
+		else if (coord > getHeight()) return getHeight()+3;
+		else return coord;
+	}
 	private double toXValue(int x) { return ((double) (x - centerX)) / scaleX; }
-	private double toYValue(int y) { return ((double) -(y - centerY) / scaleY); }
+	private double toYValue(int y) { return ((double) -(y - centerY)) / scaleY; }
 	private String toString(int a) { return String.valueOf(a); }
 	
 	// to be replaced by Parser.eval()
 	private double f(double x) {
-		return Math.cos(x);
-//		return x*x*x - 3*x;
+//		return Math.cos(x);
+		return x*x*x - 3*x;
 	}
 	private void thickLine(Graphics g, int x1, int y1, int x2, int y2) {
 		g.drawLine(x1, y1-1, x2, y2-1);
