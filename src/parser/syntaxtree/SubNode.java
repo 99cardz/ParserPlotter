c26cec57b01c9f87ea92bbb9a41988dad4754535
Hans <ali4bay@gmail.com>
2022-07-08 13:23:56 +0200

    Update Node names

--- src/parser/syntaxtree/SubNode.java
+++ src/parser/syntaxtree/SubNode.java
@@ -1,19 +1,19 @@
 package parser.syntaxtree;
 
-public class PlusNode extends BinarySyntaxNode {
-    public PlusNode(SyntaxNode left, SyntaxNode right) {
+public class SubNode extends BinarySyntaxNode {
+    public SubNode(SyntaxNode left, SyntaxNode right) {
         super(left, right);
     }
 
     public void print() {
         System.out.print("(");
         left.print();
-        System.out.print(" + ");
+        System.out.print(" - ");
         right.print();
         System.out.print(")");
     }
 
     public double eval(double x) {
-        return left.eval(x) + right.eval(x);
+        return left.eval(x) - right.eval(x);
     }
 }
