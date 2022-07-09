177fbebe9cf88c50d83e7aaeb61fa15ed0c130c6
Hans <ali4bay@gmail.com>
2022-07-10 01:45:04 +0200

    Initial syntactical analysis implementation

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import parser.syntaxtree.*;
+
 public class Parser {
 
     public static List<Token> lex(final String string) throws SyntaxException {
@@ -97,5 +99,135 @@ public class Parser {
         catch (Exception e) {
             nextToken = null;
         }
+    }
+
+    public SyntaxNode buildSyntaxTree(final String string) {
+        try {
+            tokens = lex(string);
+        }
+        catch (SyntaxException e) {
+            System.out.println("Syntax error: '" + e.getString() + "' at index " + e.getStartIndex() + " to " + e.getEndIndex());
+            return null;
+        }
+        tokenIndex = 0;
+
+        scanToken();
+
+        try {
+            SyntaxNode root = parseExpr();
+
+            return root;
+        }
+        catch(SyntaxException e) {
+            System.out.println("Error while building syntax tree!");
+            return null;
+        }
+    }
+
+    private SyntaxNode parseExpr() throws SyntaxException {
+        SyntaxNode a = parseTerm();
+
+        while(true) {
+            if(nextToken.getType() == TokenType.PLUS) {
+                scanToken();
+                SyntaxNode b = parseTerm();
+                a = new AddNode(a, b);
+            }
+            else if(nextToken.getType() == TokenType.MINUS) {
+                scanToken();
+                SyntaxNode b = parseTerm();
+                a = new SubNode(a, b);
+            }
+            else {
+                return a;
+            }
+        }
+    }
+
+    private SyntaxNode parseTerm() throws SyntaxException {
+        SyntaxNode a = parsePow();
+
+        while(true) {
+            if(nextToken.getType() == TokenType.MUL) {
+                scanToken();
+                SyntaxNode b = parsePow();
+                a = new MultNode(a, b);
+            }
+            else if(nextToken.getType() == TokenType.DIV) {
+                scanToken();
+                SyntaxNode b = parsePow();
+                a = new DivNode(a, b);
+            }
+            else {
+                return a;
+            }
+        }
+    }
+
+    private SyntaxNode parsePow() throws SyntaxException {
+
+
+        // Check for negation
+        if(nextToken.getType() == TokenType.MINUS) {
+            scanToken();
+            return new NegateNode(parsePow());
+        }
+        else {
+            SyntaxNode a = parseFact();
+
+            if (nextToken.getType() == TokenType.POW) {
+                scanToken();
+                return new PowNode(a, parsePow());
+            } else {
+                return a;
+            }
+        }
+    }
+
+    private SyntaxNode parseFact() throws SyntaxException {
+        if(nextToken.getType() == TokenType.NUMBER) {
+            return new NumberNode(nextToken.getValue());
+        }
+        else if(nextToken.getType() == TokenType.FUNC_ID) {
+            // Keep record which function it is
+            FunctionType funcType = nextToken.getFuncType();
+
+            // Check for opening parenthesis
+            scanToken();
+            if(nextToken.getType() != TokenType.PAR_OPEN) {
+                throw new SyntaxException(nextToken.getString(), nextToken.getStartIndex(), nextToken.getEndIndex());
+            }
+
+            // Parse the function argument
+            SyntaxNode a = parseExpr();
+
+            // Check for closing parenthesis
+            if(nextToken.getType() != TokenType.PAR_CLOSE) {
+                throw new SyntaxException(nextToken.getString(), nextToken.getStartIndex(), nextToken.getEndIndex());
+            }
+
+            // Build and return the node
+            switch(funcType) {
+                case COS: return new CosNode(a);
+                case LOG: return new LogNode(a);
+                case SIN: return new SinNode(a);
+                case TAN: return new TanNode(a);
+                case SQRT: return new SqrtNode(a);
+            }
+        }
+        else if(nextToken.getType() == TokenType.PAR_OPEN) {
+            scanToken();
+            SyntaxNode a = parseExpr();
+            scanToken();
+
+            return a;
+        }
+        else
+        {
+            return new VariableNode();
+        }
+
+        // This should never happen
+        return null;
     }
 }
--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -1,5 +1,7 @@
 package parser;
 
+import parser.syntaxtree.SyntaxNode;
+
 import java.util.List;
 
 public class ParserTest {
@@ -9,6 +11,8 @@ public class ParserTest {
         }
     }
 
+    private static final String[] testEpxressions = { "x + x"};
+
     public static void main(String[] args) {
         try {
             printTokenList(Parser.lex("x + x -255 * x / * sin()"));
@@ -16,5 +20,10 @@ public class ParserTest {
         catch (SyntaxException e) {
             System.out.println("Illegal symbol \"" + e.getString() + "\" at position " + e.getStartIndex());
         }
+
+        Parser parser = new Parser();
+
+        SyntaxNode root = parser.buildSyntaxTree("x + 2 + 20.0 + (-5^2)");
+        root.print();
     }
 }
