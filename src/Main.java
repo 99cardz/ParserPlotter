6732892453115634088b92d40e96e87fa72f0433
nessel <w.2nert@gmail.com>
2022-07-11 13:55:42 +0200

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
 import java.awt.Frame;
+import java.awt.Canvas;
+import gui.Window;
 
 public class Main {
 	
-	final Frame window = new Frame("Funktionsplotter");
-	
-	Main() {
-		window.add(new CanvasPlot(700, 700));
-		window.setLayout(null);    
-		window.setSize(700, 700);    
-		window.setVisible(true); 
-	}
-
 	public static void main(String[] args) {
-		
-		Main programm = new Main();
+		Window window = new Window(new Canvas());
+		window.setVisible(true); 
 	}
 
 }
