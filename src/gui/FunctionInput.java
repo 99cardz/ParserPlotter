adb0bd59d467f212b68ff1c4abe3f628bbf877f0
99cardz <jonathan.schulze.de@gmail.com>
2022-08-07 21:58:16 +0200

    merged with finishingTouchGUI

--- src/gui/FunctionInput.java
+++ src/gui/FunctionInput.java
@@ -1,38 +1,28 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.UUID;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.event.*;
 
 import canvas.CanvasPlot;
 import viewmodel.ViewModel;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class FunctionInput extends JPanel {
 
 	private static final String 	EMPTY_INPUT = "Enter a function.";
-
+	
 	// references
 	private ViewModel viewModel = ViewModel.getInstance();
 	private CanvasPlot canvas;
-
+	
     private static final Color[] colors = {
 		new Color(39, 88, 216), // blue
 		new Color(150, 30, 225), // purple
@@ -43,25 +33,26 @@ public class FunctionInput extends JPanel {
 		new Color(36, 206, 219), // cyan
 		new Color(119, 136, 119) // gray
 	};
-
+    
     private static int colorIndex = 0;
 	private Color			color		= getNextColor();
     final UUID id;
-
+	
 	public boolean			status		= true;
-
+	
 	private JTextField 		input 		= new JTextField(EMPTY_INPUT);
-	private JPanel 			colorLine = new JPanel();
+	private JPanel 			colorLine 	= new JPanel();
 	private JLabel 			label 		= new JLabel("", SwingConstants.CENTER);
 	private JPanel			right		= new JPanel(new BorderLayout());
-
+	private JButton			delete		= new JButton();
+	
 	FunctionInput(CanvasPlot cp) {
 		super(new BorderLayout());
-
+		
 		canvas = cp;
-
+		
 		id = viewModel.addFunction(color);
-
+		
 		colorLine.setBackground(color);
 		setBorder(new EtchedBorder(EtchedBorder.LOWERED));
 		input.setBorder(new EmptyBorder(0, 20, 0, 20));
@@ -95,21 +86,20 @@ public class FunctionInput extends JPanel {
 			public void removeUpdate(DocumentEvent e) {
 				insertUpdate(e);
 			}
-
+			
 			@Override
 			public void changedUpdate(DocumentEvent e) {
 				insertUpdate(e);
 			}
 		});
-
+		
 		colorLine.addMouseListener(new MouseAdapter() {
-			@Override
 			public void mouseClicked(MouseEvent e1) {
 				JFrame pickerWindow = new JFrame("Neue Farbe");
 				pickerWindow.setLayout(new BorderLayout());
-				pickerWindow.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-				pickerWindow.setSize(300, 400);
-
+				pickerWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				pickerWindow.setSize(300, 250);
+				
 				JPanel colorChange = new JPanel(new GridLayout(1,2));
 				colorChange.setPreferredSize(new Dimension(300, 80));
 				JPanel oldCol = new JPanel();
@@ -119,15 +109,15 @@ public class FunctionInput extends JPanel {
 				colorChange.add(oldCol);
 				colorChange.add(nextCol);
 				pickerWindow.add(colorChange, BorderLayout.SOUTH);
-
+				
 				JPanel sliderPanel = new JPanel(new GridLayout(3,1));
 				JPanel labelPanel = new JPanel(new GridLayout(3,1));
-
+				
 				int[] previousCol = {color.getRed(), color.getGreen(), color.getBlue()};
 				Color[] colors = {Color.red, Color.green, Color.blue};
 				JSlider[] sliders = new JSlider[3];
 				JPanel[] labels = new JPanel[3];
-
+				
  				for (int i = 0; i < 3; i++) {
  					labels[i] = new JPanel();
  					labels[i].setBackground(colors[i]);
@@ -148,55 +138,76 @@ public class FunctionInput extends JPanel {
 				pickerWindow.setVisible(true);
 			}
 		});
+		
+		delete.setBorderPainted(false);
+		delete.setContentAreaFilled(false);
+		delete.setFocusPainted(false);
+		right.setBackground(Color.WHITE);
+		try {
+			Image img = ImageIO.read(getClass().getResource("./deleteIcon.png"));
+			img = img.getScaledInstance(15, 15, Image.SCALE_SMOOTH);
+			delete.setIcon(new ImageIcon(img));
+			Image img2 = ImageIO.read(getClass().getResource("./deletePressed.png"));
+			img2 = img2.getScaledInstance(15, 15, Image.SCALE_SMOOTH);
+			delete.setPressedIcon(new ImageIcon(img2));
+		} catch (Exception ex) {
+			delete.setIcon(null);
+			delete.setText("x");
+		}
 
+		right.add(delete, BorderLayout.EAST);
 		right.add(input, BorderLayout.CENTER);
 		right.add(label, BorderLayout.SOUTH);
 		this.add(colorLine, BorderLayout.WEST);
 		this.add(right, BorderLayout.CENTER);
 	}
-
+	
 	/*
 	 * @returns: input
 	 */
 	public JTextField getInput() {
 		return input;
 	}
-
+	
+	public JButton getDeleteButton() {
+		return delete;
+	}
+	
 	/*
 	 * @returns: String contained by input
 	 */
 	public String getInputText() {
 		return input.getText();
 	}
-
+	
 	/*
 	 * @returns: true if input contains only whitespace or the empty field prompt
 	 */
 	public boolean isBlank() {
 		return input.getText().isBlank() || input.getText().equals(EMPTY_INPUT);
 	}
-
+	
 	/*
 	 * @param s: String to set input to
 	 */
 	public void setInputText(String s) {
 		input.setText(s);
 	}
-
+	
 	/*
 	 * @returns: String contained by label
 	 */
 	public String getLabelText() {
 		return label.getText();
 	}
-
+	
 	/*
 	 * @param s: String to set label to
 	 */
 	public void setLabelText(String s) {
 		label.setText(s);
 	}
-
+		
 	public static Color getNextColor() {
 		return colors[(colorIndex++) % colors.length];
 	}
